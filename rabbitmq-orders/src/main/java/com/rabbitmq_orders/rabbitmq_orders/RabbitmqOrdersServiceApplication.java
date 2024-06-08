1d8fdabbff021f4151b26a9940366d7157cdec52
Luiz Gustavo Farabello Martins <luizgufamartins@gmail.com>
2024-06-08 20:27:50 -0300

    feat: add rabbitmq api orders generator

---
+++ rabbitmq-orders/src/main/java/com/rabbitmq_orders/rabbitmq_orders/RabbitmqOrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq_orders.rabbitmq_orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqOrdersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqOrdersServiceApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-orders/src/test/java/com/rabbitmq_orders/rabbitmq_orders/RabbitmqOrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq_orders.rabbitmq_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqOrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
